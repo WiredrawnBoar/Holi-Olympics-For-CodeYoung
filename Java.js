abdaa17ba5a4eb2edc9c82fa0feb7dfd3edac062
WiredrawnBoar <99906744+WiredrawnBoar@users.noreply.github.com>
2022-03-24 20:33:18 +0000

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,12 @@
+function purchase(){
+alert("Congratulations! You have successfully Purchased This Item")
+}
+
+function add_to_basket(){
+alert("Congratulations! You have added this item to your basket")
+}
+
+function button(){
+    window.open("HTML - Main Store Page.html");
+
+}
